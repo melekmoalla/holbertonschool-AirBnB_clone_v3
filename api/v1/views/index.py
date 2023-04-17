e3e56f41367a3bf4073f3a0a84164edec5fde4e3
melekmoalla <melekmoalla2468@gmail.com>
2023-04-17 16:21:13 +0100

    add files

---
+++ api/v1/views/index.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+from api.v1.views import app_views
+from flask import Flask, jsonify
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def api_status():
+    return jsonify(status="OK")
