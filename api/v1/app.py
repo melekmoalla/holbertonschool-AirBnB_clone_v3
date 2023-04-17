e1549a673c54ca9c25d468861430b314e449bfdd
melekmoalla <melekmoalla2468@gmail.com>
2023-04-17 16:54:49 +0100

    test

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 """
 create a variable app, instance of Flask
+import storage from models
+import app_views from api.v1.views
+register the blueprint app_views to your Flask instance app
+declare a method to handle @app.teardown_appcontext that calls storage.close()
+inside if __name__ == "__main__":, run your Flask server (variable app) with:
+host = environment variable HBNB_API_HOST or 0.0.0.0 if not defined
+port = environment variable HBNB_API_PORT or 5000 if not defined
+threaded=True
 """
-
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-import os
- 
-"""
-create a variable app, instance of Flask
-"""
+import os 
 app = Flask(__name__)
-app.register_blueprint(app_views)
-
+app.register_blueprint(app_views, url_prefix="/api/v1")
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = os.getenv("HBNB_API_PORT", 5000)
