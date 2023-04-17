f4076a1606032620c5457f101a144d46167e6c9d
melekmoalla <melekmoalla2468@gmail.com>
2023-04-17 16:57:59 +0100

    test

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,7 +3,9 @@
 Create a folder views inside v1
 """
 from flask import Blueprint
-import api
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1') 
+
 from api.v1.views.index import *
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')  
+ 
