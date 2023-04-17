9fdd7d3e2f5fbb2bd7620bbedd91364f1fc0d32e
melekmoalla <melekmoalla2468@gmail.com>
2023-04-17 16:23:25 +0100

    add documentation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-
+"""
+mport app_views from api.v1.views
+create a route /status on the object
+app_views that returns a JSON: "status" ok
+"""
 from api.v1.views import app_views
 from flask import Flask, jsonify
 
