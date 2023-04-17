f11113fd3af594e923be9e99a95d69c673557b21
melekmoalla <melekmoalla2468@gmail.com>
2023-04-18 00:26:49 +0100

    add clse in file app

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,6 +11,14 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+@app.route("/")
+def hello():
+    return("mayouka")
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = os.getenv("HBNB_API_PORT", 5000)
