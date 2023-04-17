73da5ffbeea8c5737b6ddef974d4538f26e76883
melekmoalla <melekmoalla2468@gmail.com>
2023-04-17 17:03:25 +0100

    test pep8

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,10 +2,7 @@
 """
 Create a folder views inside v1
 """
+from api.v1.views.index import *
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1') 
-
-from api.v1.views.index import *
-
- 
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,6 +7,7 @@ app_views that returns a JSON: "status" ok
 from api.v1.views import app_views
 from flask import Flask, jsonify
 
-@app_views.route('/status' , strict_slashes=False)
+
+@app_views.route('/status', strict_slashes=False)
 def api_status():
     return jsonify({"status": "OK"})
