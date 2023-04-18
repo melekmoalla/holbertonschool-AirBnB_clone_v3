1766acc591a131a649b0409c45c2887de7f0256c
melekmoalla <melekmoalla2468@gmail.com>
2023-04-19 00:32:06 +0100

    test modify and add file state

---
+++ api/v1/views/states.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from models import storage
+from models.state import State
+import models
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def status():
+    slist = []
+    states = storage.all(State).values()
+    for state in states:
+        slist.append(state.to_dict())
+    return jsonify(slist)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """Retrieves a State object"""
+    states = storage.all(State)
+    key = "State."+state_id
+    a = states[key]
+    if a is None:
+        abort(404)
+    return jsonify(a.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state(state_id):
+    states = storage.all(State)
+    key = "State."+state_id
+    if key not in states:
+        abort(404)
+    a = states[key]
+    storage.delete(a)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/states/<state_id>', methods=['POST'], strict_slashes=False)
+def post(state_id):
+    states = storage.all(State)
+    key = "State."+state_id
+    if key not in states:
+        abort(400, 'Not a JSON')
+    js = request.get_json()
+    if 'name' not in js:
+        abort(400, 'Missing name')
+    state = State(**js)
+    storage.new(state)
+    storage.save()
+    return jsonify(state.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def put(state_id):
+    states = storage.all(State)
+    key = "State."+state_id
+    if key not in states:
+        abort(400, 'Not a JSON')
+    a = states[key]
+    js = request.get_json()
+    m = a.__dict__
+    for i in js:
+        if i not in ["id", "created_at", "updated_at"]:
+            m[i] = js[i]
+    storage.save()
+    return jsonify(a.to_dict())
