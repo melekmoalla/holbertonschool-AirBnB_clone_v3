b37696b4dcc103f01f7166ef015882b15968d14d
melekmoalla <melekmoalla2468@gmail.com>
2023-04-21 14:30:01 +0100

    fix pep8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -85,4 +85,3 @@ class FileStorage:
             if key in self.__objects:
                 return self.__objects[key]
         return None
-    
